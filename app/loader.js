6f6fd476b1369e8f92db3743d394c30dfcb460d0
321Proteus <bgproteus@gmail.com>
2024-01-25 17:29:12 +0100

    Creation of loader.js

---
+++ app/loader.js
@@ -0,0 +1,29 @@
+var jsmedia = window.jsmediatags;
+
+function loadMetadata(file) {
+    return new Promise(function(resolve, reject) {
+
+        jsmedia.read(file, {
+            onSuccess: function(tag) {
+                console.log(tag.tags)
+                resolve(tag.tags);
+            },
+            onError: function(err) {
+                reject(err);
+            }
+        });
+    });
+}
+
+function getCover(data, fmt) {
+    var container = Array.from(document.getElementsByClassName("cover"))[0];
+
+    var cover = document.createElement("img")
+    var base64 = "";
+  
+    for (const el of data) base64 += String.fromCharCode(el);
+  
+    cover.src = `data:${fmt};base64,${window.btoa(base64)}`;
+    
+    container.append(cover);
+}
