d2dbfa944c9fdaf8ba83479bdbf3d3aa449ee971
321Proteus <bgproteus@gmail.com>
2024-02-06 16:51:54 +0100

    Added saving config to localStorage

--- app/settings.js
+++ app/settings.js
@@ -1,16 +1,65 @@
+var settings; getSettings();
+document.documentElement.style.setProperty("--accent", settings.accentColor);
+
+var saveAccent = false;
+
+function getSettings() {
+    settings = JSON.parse(localStorage.getItem("iqplayer-settings"));
+
+    // TODO: Why does it need to be parsed two times?
+
+    // if (!settings) settings = {
+    //     accentColor: "#75DD82",
+    //     alignMode: true
+    // };  
+}
+
+function saveSettings() {
+    localStorage.setItem("iqplayer-settings", JSON.stringify(settings));
+
+}
+
 function checkbox(el) {
 
     if (el.checked == true) {
-        console.log("checked")
+        switch (el.id) {
+            case "save-accent": {
+                saveAccent = !saveAccent;
+                document.getElementById("save-accent-preview").innerText = saveAccent;
+            }
+            break;
+
+            default:
+            break;
+        }
     }
     else {
-        console.log("unchecked");
+        switch (el.id) {
+            case "save-accent": {
+                saveAccent = !saveAccent;
+                document.getElementById("save-accent-preview").innerText = saveAccent;
+            }
+            break;
+
+            default:
+            break;
+        }
     }
+}
+
+function getAccent() {
+    if (saveAccent) {
+        getSettings()
+        return settings.accentColor;
+    }
+    else return settings.accentColor;
 }
 
 function colorScanner() {
     var input = document.getElementById("color");
     input.addEventListener("input", function() {
+
+        getSettings()
 
         var text = this.value.toUpperCase();
         if (!text.startsWith("#")) text = "#" + text;        
@@ -18,12 +67,15 @@ function colorScanner() {
         const regex = new RegExp("([A-F0-9]{6}|[A-F0-9]{3})$")
 
         if (!regex.test(text)) {
-            document.documentElement.style.setProperty("--accent", "#75DD82");
-            document.getElementById("color-info").innerText = "Invalid Hex colour";
+            document.documentElement.style.setProperty("--accent", settings.accentColor);
+            document.getElementById("accent-color-preview").innerText = "Invalid Hex colour";
         }
         else {
             document.documentElement.style.setProperty("--accent", text);
-            document.getElementById("color-info").innerText = text;
+            document.getElementById("accent-color-preview").innerText = text;
+            settings.accentColor = text;
+            if (saveAccent)
+            saveSettings();
         }
 
     })    
